adaafa596dbe13ecaec68503b0e3843160b973a9
3li-3bdullah <PROGRMMER7@gmail.com>
2023-03-03 09:46:35 +0200

    Added Data Type

--- lib/constants.dart
+++ lib/constants.dart
@@ -1,8 +1,8 @@
 import 'package:flutter/material.dart';
 
-const kBackgroundcolor = Color(0xFFF1EFF1);
-const kPrimaryColor = Color(0xFF219ebc);
-const kSecendaryColor = Color(0xFFfcca46);
-const kTextColor = Color(0xFF023043);
-const kTextLightColor = Color(0xFF747474);
-const kDefultPadding = 20.0;
+const Color kBackgroundcolor = Color(0xFFF1EFF1);
+const Color kPrimaryColor = Color(0xFF219ebc);
+const Color kSecendaryColor = Color(0xFFfcca46);
+const Color kTextColor = Color(0xFF023043);
+const Color kTextLightColor = Color(0xFF747474);
+const double kDefultPadding = 20.0;
